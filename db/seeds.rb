74662e1276828f5c87c967cc6e3221aaafba4811
Lauren Smolenski <lauren@smolenski.net.nz>
2014-11-15 16:01:03 +1100

    I got create category working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 parsed = JSON.parse(File.read("db/the_iconic_data_feed.json"))
 
-parsed.each do |item|
+Item.destroy_all
+Category.destroy_all
 
+parsed.each do |item|
 
 	new_item = Item.new
 	new_item.merchant_id = item["MerchantId"]
@@ -16,6 +18,21 @@ parsed.each do |item|
 	new_item.keywords = item["Keywords"]
 	new_item.image_url50 = item["Image50"]
 	new_item.image_url400 = item["Image400"]
-	new_item.save
 
+
+  category_s = item["Category"]
+	categories_array = category_s.split(' > ')
+	parent_id = nil
+	categories_array.each do |category|
+	  if Category.where(:child => category).length > 0
+	    existing = Category.where(:child => category)
+	    parent_id = existing.first.id
+	  else
+	    new_category = Category.create(:child => category, :parent => parent_id)
+	    parent_id = new_category.id
+	  end
+	end
+
+  new_item.category_id = parent_id
+  new_item.save
 end
