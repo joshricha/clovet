218f82f30bfd6a9ed71aa80712fbc619973f6ae7
Jam Creencia <jrcreencia@gmail.com>
2014-11-15 14:23:05 +1100

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,20 @@
-@parsed = JSON.parse(File.read("the_iconic_data_feed.json"))
+parsed = JSON.parse(File.read("the_iconic_data_feed.json"))
+
+parsed.each do |item|
+
+	
+	new_item = Item.new
+	new_item.merchant_id = item["MerchantId"]
+	new_item.sku = item["SKU"]
+	new_item.name = item["Name"]
+	new_item.merchant_url = item["Url"]
+	new_item.price = item["Price"]
+	new_item.price_sale = item["PriceSale"]
+	new_item.brand = item["Brand"]
+	new_item.color = item["Colour"]
+	new_item.gender = item["Gender"]
+	new_item.keywords = item["Keywords"]
+	# new_item.image_url50 = item[]
 
 
+end
