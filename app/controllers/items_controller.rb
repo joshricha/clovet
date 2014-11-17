9c2a92b24868d9b1efa3d49ff272779e8263bb93
Ji Lee <gafrong@gmail.com>
2014-11-18 10:11:31 +1100

    scrapped search for a different implementation

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,12 +5,6 @@ class ItemsController < ApplicationController
 
     offset = rand(Item.count)
     @rand_record = Item.offset(offset).first.id
-    
-    if !params[:search].present?
-      @items = Item.all
-    else
-      @items = Item.where("content ilike ?", "%#{params[:search]}%")
-    end
   end
 
   def show
