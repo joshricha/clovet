8cbb8e24b94e5167053eca8aa5fc28c9eac06253
Ji Lee <gafrong@gmail.com>
2014-11-18 17:00:14 +1100

    adding color

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,7 +9,6 @@ class ItemsController < ApplicationController
 
   def show
     @user = current_user
-
     @item = Item.find(params[:id])
 
     first = Item.first.id
@@ -64,5 +63,6 @@ class ItemsController < ApplicationController
 
     render '/items/category/show.html.erb'
   end
+
 
 end
